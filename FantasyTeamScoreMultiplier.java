89ad92de8ab8cb96ebc72cd8e69fb81e313a8d40
Azam-2006 <ma3968@srmist.edu.in>
2026-09-19 00:19:40 +0530

    Create FantasyTeamScoreMultiplier.java

---
+++ FantasyTeamScoreMultiplier.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FantasyTeamScoreMultiplier {
+
+    static void applyMultipliers(double[] playerScores,
+                                 int captainIndex,
+                                 int viceCaptainIndex) {
+
+        // Captain gets 2x points
+        playerScores[captainIndex] =
+                playerScores[captainIndex] * 2;
+
+        // Vice-Captain gets 1.5x points
+        playerScores[viceCaptainIndex] =
+                playerScores[viceCaptainIndex] * 1.5;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of players: ");
+        int n = sc.nextInt();
+
+        double[] scores = new double[n];
+
+        System.out.println("Enter player scores:");
+
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextDouble();
+        }
+
+        System.out.print("Enter captain index: ");
+        int captainIndex = sc.nextInt();
+
+        System.out.print("Enter vice-captain index: ");
+        int viceCaptainIndex = sc.nextInt();
+
+        applyMultipliers(scores, captainIndex, viceCaptainIndex);
+
+        System.out.println("Updated Scores: "
+                + Arrays.toString(scores));
+
+        sc.close();
+    }
+}
